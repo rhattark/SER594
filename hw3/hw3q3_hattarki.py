bb268c60ae455d34df1edea0435fc3cd3d3c5a41
Rhishabh Hattarki <rhattark@asu.edu>
2023-10-11 07:14:24 -0700

    hw3 inital commit

---
+++ hw3/hw3q3_hattarki.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+__author__ = "Rhishabh Hattarki"
+__date__ = "11 October 2023"
+__assignment = "SER594: Homework 3 Q3 Programming"
+
+
+def do_stuff(input_filename):
+    """
+    See homework assignment.
+    :param input_filename: Filename for input datafile.
+    """
+
+    # TODO
+    pass
+
+
+if __name__ == '__main__':
+    filename = "HW03_Kaggle_Youtube_DataScience.csv"
+    do_stuff(filename)
---
+++ hw3/hw3q3_template.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+__author__ = "TODO"
+__date__ = "TODO"
+__assignment = "SER*94: Homework 3 Q3 Programming"
+
+
+def do_stuff(input_filename):
+    """
+    See homework assignment.
+    :param input_filename: Filename for input datafile.
+    """
+
+    # TODO
+    pass
+
+
+if __name__ == '__main__':
+    filename = "HW03_Kaggle_Youtube_DataScience.csv"
+    do_stuff(filename)
