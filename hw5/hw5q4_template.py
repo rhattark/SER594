803b4c8e7b11551a125655d7c1e00cf2a15995bd
Rhishabh Hattarki <rhattark@asu.edu>
2023-11-06 09:13:14 -0700

    hw5 init

---
+++ hw5/hw5q4_template.py
@@ -0,0 +1,23 @@
+# import pandas as pd
+# from sklearn.linear_model import LinearRegression
+# from sklearn.utils import shuffle
+import numpy as np
+
+__author__ = "TODO"
+__date__ = "TODO"
+__assignment = "SER*94: Homework 5 Q4 Programming"
+
+
+def build_models(input_filename, fraction_training=.8):
+    """
+    See homework assignment.
+    :param input_filename: Filename for input datafile.
+    """
+
+    # TODO
+    pass
+
+
+if __name__ == '__main__':
+    filename = "q4_data.csv"
+    build_models(filename)
---
+++ hw5/hw5q5_template.py
@@ -0,0 +1,21 @@
+# import pandas as pd
+import numpy as np
+
+__author__ = "TODO"
+__date__ = "TODO"
+__assignment = "SER*94: Homework 5 Q5 Programming"
+
+
+def run_gd(input_filename, fraction_training=.8):
+    """
+    See homework assignment.
+    :param input_filename: Filename for input datafile.
+    """
+
+    # TODO
+    pass
+
+
+if __name__ == '__main__':
+    filename = "q5_data.csv"
+    run_gd(filename)
