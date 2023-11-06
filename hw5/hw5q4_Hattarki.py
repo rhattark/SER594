306bb0f12d60dbf05db9035c2f414195332ae5fc
Rhishabh Hattarki <rhattark@asu.edu>
2023-11-06 10:48:51 -0700

    custom linear regression weights

--- hw5/hw5q4_Hattarki.py
+++ hw5/hw5q4_Hattarki.py
@@ -7,6 +7,15 @@ __author__ = "Rhishabh Hattarki"
 __date__ = "6 November 2023"
 __assignment = "SER594: Homework 5 Q4 Programming"
 
+def custom_linear_regression(A_, b_):
+    A_with_ones = np.column_stack((A_, np.ones(A_.shape[0])))
+    A_transpose = np.transpose(A_with_ones)
+    A_transpose_dot_A = np.dot(A_transpose, A_with_ones)
+    inverse = np.linalg.inv(A_transpose_dot_A)
+    inverse_dot_A_transpose = np.dot(inverse, A_transpose)
+    weights = np.dot(inverse_dot_A_transpose, b_)
+
+    return weights
 
 def build_models(input_filename, fraction_training=.8):
     """
@@ -21,7 +30,14 @@ def build_models(input_filename, fraction_training=.8):
 
     train_df = shuffled_df.iloc[:split_idx]
     test_df = shuffled_df.iloc[split_idx:]
-    print(len(train_df), len(test_df))
+
+    X_train = train_df.iloc[:, 0]
+    X_test = test_df.iloc[:, 0]
+    y_train = train_df.iloc[:, -1]
+    y_test = test_df.iloc[:, -1]
+
+    custom_weights = custom_linear_regression(X_train.values, y_train.values)
+    print(custom_weights)
     
     pass
 
